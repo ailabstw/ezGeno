a6ca1000681f394694e7727d5371e22bce3a469e
tsung-ting.hseih <tsung-ting.hsieh@ailabs.tw>
2021-07-10 14:08:26 +0000

    load model bug fix

--- example/AcEnhancer/run.sh
+++ example/AcEnhancer/run.sh
@@ -4,5 +4,5 @@ comment
 <<comment
 2.run ezgeno code
 comment
-python3 ../../ezgeno/ezgeno.py --trainFileList /volume/tsungting/ezgeno/eNAS/epigenome/data/h1hesc_dnase.training.score,/volume/tsungting/ezgeno/eNAS/epigenome/data/h1hesc_dnase.training_input.sequence  --trainLabel /volume/tsungting/ezgeno/eNAS/epigenome/data/h1hesc_dnase.training_label --testFileList /volume/tsungting/ezgeno/eNAS/epigenome/data/h1hesc_dnase.validation.score,/volume/tsungting/ezgeno/eNAS/epigenome/data/h1hesc_dnase.validation_input.sequence --testLabel /volume/tsungting/ezgeno/eNAS/epigenome/data/h1hesc_dnase.validation_label --cuda 0  --save example.model
+python3 ../../ezgeno/ezgeno.py --trainFileList ./h1hesc_dnase.training.score,./h1hesc_dnase.training_input.sequence  --trainLabel ./h1hesc_dnase.training_label --testFileList ./h1hesc_dnase.validation.score,./h1hesc_dnase.validation_input.sequence --testLabel ./h1hesc_dnase.validation_label --cuda 0  --save example.model
 <<comment
